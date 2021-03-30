93da5919d5357a63f510099c4d154fab1922c2bd
Dexter <dex429.dc@gmail.com>
2021-03-29 21:59:12 -0400

    commit tree ab42ddb6a8832b444a46912ccea2ca9e54519599

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/" do
+
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        @string = ring.pig(params[:user_phrase])
+        erg :piglatinize
+    end
 end
